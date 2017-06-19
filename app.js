25dadbf16de0cf28b4ae06bd3aa01a0cde586064
chuntaomou <chuntaomou@users.noreply.github.com>
2017-06-19 11:49:56 +0100

    change app,js

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var sql = require('mssql');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+//var insertform = require('./routes/insertform');
 
+var upload = multer();
 var app = express();
 
 // view engine setup
@@ -17,13 +21,62 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());// for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
+app.use(upload.array());// for parsing multipart/ form-data
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//sql server
+var dbConfig = {
+    user:  'ucabcm2',
+    password: 'Mct.niub74110',
+    server: 'nuffieldhealth20170610.database.windows.net',
+    options: {
+      encrypt: true,
+      database: 'nuffieldhealth_db'
+    }
+};
+
+//get connect to sql server and execute query
+var connection = new sql.Connection(dbConfig);
+connection.connect().then(function(){
+  console.log("connected");
+}).catch(function(err){
+  console.log(err);
+});
+
+//functions to execute queries
+var executeQuery=function(query,res){
+	var request=new sql.Request(connection);
+	request.query(query,function(err,result){
+		if (err){
+			console.log("Error while querying database :- " + err);
+			return res.send(err);
+		}else{
+			console.log(result);
+			sql.close();
+			res.send(result);
+		}
+	});
+}
+
 app.use('/', index);
 app.use('/users', users);
+//app.use('/insertform',insertform);
+
+app.post('/insert',function(req,res){
+	var query = "INSERT INTO Items (Name, Description, Price) VALUES ('"+req.body.name+"','"+req.body.description+"',"+req.body.price+")";
+	executeQuery(query,res);
+});
+app.post('/delete',function(req,res){
+	res.send("recieved your request!");
+	console.log(req.body.name);
+});
+app.post('/update',function(req,res){
+	res.send("recieved your request!");
+	console.log(req.body.name);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
